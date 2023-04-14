59fa4d5ce956043cf2432277110a625babce3656
louispaulet <louispaulet13@gmail.com>
2023-04-14 16:43:36 +0200

    add publication data to viewer and to dropdown

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const qagSelector = document.getElementById('qagSelector');
   const qagContent = document.getElementById('qagContent');
+  
+  function extractDate(json) {
+  let dateJO = 'Unknown';
+
+  if (json.question.indexationAN.minAttribs &&
+      json.question.indexationAN.minAttribs.minAttrib &&
+      json.question.indexationAN.minAttribs.minAttrib.infoJO &&
+      json.question.indexationAN.minAttribs.minAttrib.infoJO.dateJO) {
+    dateJO = json.question.indexationAN.minAttribs.minAttrib.infoJO.dateJO;
+  } else if (json.question.textesReponse.texteReponse.infoJO &&
+             json.question.textesReponse.texteReponse.infoJO.dateJO) {
+    dateJO = json.question.textesReponse.texteReponse.infoJO.dateJO;
+  }
+
+  return dateJO;
+}
+
+function jsonToHTML(json) {
+  const dateJO = extractDate(json);
+
+  const qagDate = document.createElement('h3');
+  qagDate.textContent = `Date of publication: ${dateJO === 'Unknown' ? dateJO : new Date(dateJO).toLocaleDateString()}`;
+  qagContent.appendChild(qagDate);
+
+  const qagTitle = document.createElement('h2');
+  qagTitle.textContent = json.question.indexationAN.rubrique;
+  qagContent.appendChild(qagTitle);
+
+  const qagText = document.createElement('div');
+  qagText.innerHTML = json.question.textesReponse.texteReponse.texte;
+  qagContent.appendChild(qagText);
+}
+
+async function fetchQAGList() {
+  const filenames = await fetchQAGFilenames();
+  const qagListPromises = filenames.map(async (filename) => {
+    const json = await loadQAG(`qag_json/${filename}`);
+    const dateJO = extractDate(json);
+    return {
+      date: dateJO === 'Unknown' ? dateJO : new Date(dateJO).toLocaleDateString(),
+      title: filename.slice(0, -5),
+      filename,
+      file: `qag_json/${filename}`,
+    };
+  });
+  const qagList = await Promise.all(qagListPromises);
+  qagList.sort((a, b) => a.date === 'Unknown' ? 1 : b.date === 'Unknown' ? -1 : new Date(a.date) - new Date(b.date));
+  return qagList;
+}
+
 
   async function loadQAG(file) {
     const response = await fetch(file);
@@ -12,19 +62,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     qagList.forEach(qag => {
       const option = document.createElement('option');
       option.value = qag.file;
-      option.textContent = qag.title;
+      option.textContent = `${qag.date} - ${qag.title} - ${qag.filename}`;
       qagSelector.appendChild(option);
     });
-  }
-
-  function jsonToHTML(json) {
-    const qagTitle = document.createElement('h2');
-    qagTitle.textContent = json.question.indexationAN.rubrique;
-    qagContent.appendChild(qagTitle);
-
-    const qagText = document.createElement('div');
-    qagText.innerHTML = json.question.textesReponse.texteReponse.texte;
-    qagContent.appendChild(qagText);
   }
 
   function clearQAGContent() {
@@ -36,15 +76,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     const text = await response.text();
     const filenames = text.split(/\s+/).filter((filename) => filename.trim() !== '');
     return filenames;
-  }
-
-  async function fetchQAGList() {
-    const filenames = await fetchQAGFilenames();
-    const qagList = filenames.map((filename) => ({
-      title: filename.slice(0, -5),
-      file: `qag_json/${filename}`,
-    }));
-    return qagList;
   }
 
   const qagList = await fetchQAGList();
